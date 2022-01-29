8c69463ac359c2ac8e2ab52ef1099271b7da074f
rs280 <rishabhsahoo1@gmail.com>
2022-01-28 22:57:47 -0800

    adding everything

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -12,22 +12,22 @@ public class Driver {
 		// Build Automobile Object from a file.
 		String FordZTWAutomobileKey = buildAutoInterface.buildAuto("FordZTW.txt");
 		String BMW320iAutomobileKey = buildAutoInterface.buildAuto("BMW320i.txt");
-		if (FordZTWAutomobileKey != null) {
+		if (BMW320iAutomobileKey != null) {
 			// Print attributes before serialization
-			buildAutoInterface.printAuto(FordZTWAutomobileKey);
+			buildAutoInterface.printAuto(BMW320iAutomobileKey);
 			// update an options set's name
-			buildAutoInterface.updateOptionSetName(FordZTWAutomobileKey, "Color", "Colors");
+			buildAutoInterface.updateOptionSetName(BMW320iAutomobileKey, "Color", "Colors");
 			// update an options set's option price
-			buildAutoInterface.updateOptionPrice(FordZTWAutomobileKey, "Transmission", "automatic", 50);
+			buildAutoInterface.updateOptionPrice(BMW320iAutomobileKey, "Transmission", "automatic", 50);
 			// choose a transmission option
-			buildAutoInterface.setOptionChoice(FordZTWAutomobileKey, "Transmission", "manual");
+			buildAutoInterface.setOptionChoice(BMW320iAutomobileKey, "Transmission", "manual");
 			// Serialize the object
-			buildAutoInterface.serialize(FordZTWAutomobileKey, "FordZTW.data");
+			buildAutoInterface.serialize(BMW320iAutomobileKey, "BMW320i.data");
 		} else {
 			System.out.println("Could not build automobile");
 		}
 		// Deserialize the object and read it into memory.
-		String FordZTWAutomobileKey2 = buildAutoInterface.deserialize("FordZTW.data");
+		String FordZTWAutomobileKey2 = buildAutoInterface.deserialize("BMW320i.data");
 		// Print new attributes
 		if (FordZTWAutomobileKey2 != null) {
 			// Print attributes after deserialization
